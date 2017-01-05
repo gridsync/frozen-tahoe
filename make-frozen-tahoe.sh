8a00a12a90ad0f7904649316318c201f0424ecd7
Chris Wood <crwood@gmail.com>
2017-01-05 17:19:47 -0500

    Fallback to 'pip install --user'

--- make-frozen-tahoe.sh
+++ make-frozen-tahoe.sh
@@ -3,7 +3,7 @@
 # Build frozen Tahoe-LAFS on OS X - virtualenv edition
 #
 
-pip install --upgrade pip virtualenv
+pip install --upgrade pip virtualenv || pip install --user --upgrade pip virtualenv
 
 virtualenv --clear --python=python2 build/venv
 source build/venv/bin/activate
